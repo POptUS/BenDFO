c1b7901dc256c9b9c927861733cccf5642e9e1fa
Jeffrey Larson <jmlarson@anl.gov>
2023-07-31 12:12:54 -0500

    Adding test

---
+++ py/evaluate_all_starting_points.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import numpy as np
+import scipy as sp
+
+from dfoxs import dfoxs
+from calfun import calfun
+
+np.seterr("raise")
+
+
+def doit():
+    dfo = np.loadtxt("./cutest/dfo.txt")
+
+    for row, (nprob, n, m, factor_power) in enumerate(dfo):
+        n = int(n)
+        m = int(m)
+
+        X0 = dfoxs(n, nprob, int(10**factor_power)).T
+        out = calfun(X0, m, int(nprob), "smooth", 0, vecout=True)
+
+
+if __name__ == "__main__":
+    doit()
