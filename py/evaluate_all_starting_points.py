e4f9bb5f132a5c1ab619691031383e65eeec8e27
Jeffrey Larson <jmlarson@anl.gov>
2023-09-26 10:27:16 -0500

    Adding more calfun probtypes

--- py/evaluate_all_starting_points.py
+++ py/evaluate_all_starting_points.py
@@ -13,12 +13,13 @@ from calfun import calfun
 def doit():
     dfo = np.loadtxt("../data/dfo.dat")
 
-    for row, (nprob, n, m, factor_power) in enumerate(dfo):
-        n = int(n)
-        m = int(m)
+    for probtype in  ['absnormal', 'absuniform', 'abswild', 'noisy3', 'nondiff', 'relnormal', 'reluniform', 'relwild', 'smooth', 'wild3']:
+        for row, (nprob, n, m, factor_power) in enumerate(dfo):
+            n = int(n)
+            m = int(m)
 
-        X0 = dfoxs(n, nprob, int(10**factor_power)).T
-        out = calfun(X0, m, int(nprob), "smooth", 0, vecout=True)
+            X0 = dfoxs(n, nprob, int(10**factor_power)).T
+            out = calfun(X0, m, int(nprob), probtype, 0, vecout=True)
 
 
 if __name__ == "__main__":
