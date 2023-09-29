Fixing indexing error in nprob==22 calculation

py/evaluate_all_starting_points.py
@@ -10,7 +10,7 @@ from dfoxs import dfoxs
 
 
 dfo = np.loadtxt("../data/dfo.dat")
-probtypes = ["absnormal", "absuniform", "abswild", "noisy3", "nondiff", "relnormal", "reluniform", "relwild", "smooth", "wild3"];
+probtypes = ["absnormal", "absuniform", "abswild", "noisy3", "nondiff", "relnormal", "reluniform", "relwild", "smooth", "wild3"]
 
 Results = {}
 
@@ -22,11 +22,11 @@ for p, probtype in enumerate(probtypes):
         X0 = dfoxs(n, nprob, int(10**factor_power)).T
         [y, F, G, J] = calfun(X0, m, int(nprob), probtype, gradout=True)
 
-        Results["prob_" + str(p+1) + "_" + str(row+1)] = {}
-        Results["prob_" + str(p+1) + "_" + str(row+1)]["X0"] = X0
-        Results["prob_" + str(p+1) + "_" + str(row+1)]["y"] = y
-        Results["prob_" + str(p+1) + "_" + str(row+1)]["F"] = F
-        Results["prob_" + str(p+1) + "_" + str(row+1)]["G"] = G
-        Results["prob_" + str(p+1) + "_" + str(row+1)]["J"] = J
+        Results["prob_" + str(p + 1) + "_" + str(row + 1)] = {}
+        Results["prob_" + str(p + 1) + "_" + str(row + 1)]["X0"] = X0
+        Results["prob_" + str(p + 1) + "_" + str(row + 1)]["y"] = y
+        Results["prob_" + str(p + 1) + "_" + str(row + 1)]["F"] = F
+        Results["prob_" + str(p + 1) + "_" + str(row + 1)]["G"] = G
+        Results["prob_" + str(p + 1) + "_" + str(row + 1)]["J"] = J
 
 sp.io.savemat("fvec_and_gradients_at_starting_values_python.mat", Results)