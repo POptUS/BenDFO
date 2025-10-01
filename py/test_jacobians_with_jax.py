1b2206607e884a68aa2f343c466a7a109ec46070
Jeffrey Larson <jmlarson@anl.gov>
2025-10-01 14:12:53 -0500

    Testing all outputs with/without jax

--- py/test_jacobians_with_jax.py
+++ py/test_jacobians_with_jax.py
@@ -29,7 +29,7 @@ def compare_jacobians(nprob, m, n, x0):
     rel_diff = diff / (np.linalg.norm(J_fd) + 1e-12)
 
     print(f"nprob={nprob:2d}, m={m:3d}, n={n:2d} | abs diff: {diff:.2e}, rel diff: {rel_diff:.2e}")
-    if diff > 2e-6 and rel_diff > 2e-6:
+    if diff > 6e-7 and rel_diff > 6e-7:
         raise Exception("Difference is too large")
 
 
