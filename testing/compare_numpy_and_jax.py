ede54dc496bd2a585ebbe17d939fe465d02d6979
Jeffrey Larson <jmlarson@anl.gov>
2025-10-07 13:17:21 -0500

    black

--- testing/compare_numpy_and_jax.py
+++ testing/compare_numpy_and_jax.py
@@ -5,7 +5,7 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 
-sys.path.append('../py')
+sys.path.append("../py")
 from calfun import calfun
 from dfovec_jax import dfovec_jax
 from dfoxs import dfoxs
